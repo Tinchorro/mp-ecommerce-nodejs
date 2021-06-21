98f8f17225b2af8e265e068d68c805fdaa24b5e0
emymatupatin <martin.roman@estudiantes.utec.edu.uy>
2021-06-21 16:25:01 -0300

    Comenzando por el front

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 
 var app = express();
+
+
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'PROD_ACCESS_TOKEN'
+});
+
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
