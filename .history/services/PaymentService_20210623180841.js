4204f07d415ef257e333282e574059d0a78325ca
emymatupatin <martin.roman@estudiantes.utec.edu.uy>
2021-06-23 18:16:03 -0300

    Moneda en uruguayos

--- .history/services/PaymentService_20210623180841.js
+++ .history/services/PaymentService_20210623180841.js
@@ -23,7 +23,7 @@ class PaymentService {
         picture_url: "https://www.facebook.com/100673438542114/photos/a.100678528541605/100674671875324",
         category_id: "1234",
         quantity: parseInt(unit),
-        currency_id: "UYU",
+        currency_id: "ARS",
         unit_price: parseFloat(price)
       }
     ];
