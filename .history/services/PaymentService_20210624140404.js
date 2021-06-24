7bb1c6882df30f9631f1324ac73321f2bd661ab6
emymatupatin <martin.roman@estudiantes.utec.edu.uy>
2021-06-24 14:05:25 -0300

    Cambio description y moneda

--- .history/services/PaymentService_20210624140404.js
+++ .history/services/PaymentService_20210624140404.js
@@ -23,17 +23,17 @@ class PaymentService {
         picture_url: "https://www.facebook.com/100673438542114/photos/a.100678528541605/100674671875324",
         category_id: "1234",
         quantity: parseInt(unit),
-        currency_id: "UYU",
+        currency_id: "ARS",
         unit_price: parseFloat(price)
       }
     ];
 
     const preferences = {
       items,
-      render: {
+      render = {
         container: '.cho-container',
         label: 'Pagar la compra',
-      },
+      };
       external_reference: "martin.roman.mendy@gmail.com",
       payer: {
         name: "Lalo",
--- .history/services/PaymentService_20210624140410.js
+++ .history/services/PaymentService_20210624140410.js
@@ -23,7 +23,7 @@ class PaymentService {
         picture_url: "https://www.facebook.com/100673438542114/photos/a.100678528541605/100674671875324",
         category_id: "1234",
         quantity: parseInt(unit),
-        currency_id: "UYU",
+        currency_id: "ARS",
         unit_price: parseFloat(price)
       }
     ];
@@ -33,7 +33,7 @@ class PaymentService {
       render: {
         container: '.cho-container',
         label: 'Pagar la compra',
-      },
+      };
       external_reference: "martin.roman.mendy@gmail.com",
       payer: {
         name: "Lalo",
